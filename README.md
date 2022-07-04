5aa974677c1e5c0ce294bf68c9873693163b3d08
NangTuong <nguyen.brad@outlook.com>
2022-07-04 00:53:13 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Employee-Tracker
+
+## Purpose
+A command-line application that manages a company's employee database.
+
+## Built With
+* Node.js
+* JavaScript
+* Inquirer
+* MySQL
+
+## Website Link
+https://github.com/NangTuong/Employee-Tracker
+
+## Contributions
+Made By: Bradley Nguyen
+
+## Screen Shots
+![screenshot](./images/Screenshot%20(9).png)
+![screenshot](./images/Screenshot%20(10).png)
+
+## Walkthrough video 
+https://drive.google.com/file/d/13kb_deDvNBd1V9Os5W4grgudXEiGUYQe/view
--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employeeRole;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
@@ -7,12 +7,12 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE employeeRole (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(9,2),
     department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    --CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -21,6 +21,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    --CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    --CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
